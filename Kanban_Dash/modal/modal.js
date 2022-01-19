ab720f8ff3089aedbe05a56e810adbb3caffade2
quarentyne <peresada1997@gmail.com>
2022-01-19 21:50:41 +0200

    added modal window

--- Kanban_Dash/modal/modal.js
+++ Kanban_Dash/modal/modal.js
@@ -1,6 +1,12 @@
 'use strict'
+window.addEventListener('scroll', () => {
+    console.log(scrollY);
+    document.querySelector('.modalWrapper').style.top = `${scrollY}` + 'px';
+});
+
 
 function openModal() {
+    document.body.style.overflow = "hidden"
 
     let modalWindow = document.querySelector('.modalWrapper');
 
@@ -15,13 +21,14 @@ function openModal() {
 
     butAgree.addEventListener('click', function () {
         modalWindow.classList.remove('active');
+        document.body.style.overflow = "visible";
     })
 
     let backBlack = modalWindow.querySelector('.backBlack');
 
     backBlack.addEventListener('click', function () {
         modalWindow.classList.remove('active');
+        document.body.style.overflow = "visible";
     })
 
 }
-export { openModal };
